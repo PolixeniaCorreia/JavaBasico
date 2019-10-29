b11734e430c2f9d03b3923e6a8571f2ab7fca60f
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-10-29 15:48:14 -0300

    Arrays - Exerc 02

---
+++ curso-java-basico/src/com/poli/cursojava/aula19/labs/Exerc02.java
@@ -0,0 +1,36 @@
+package com.poli.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exerc02 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int [] vA = new int [8];
+		int [] vB = new int [vA.length];
+
+		for(int i = 0; i < vA.length; i++) {
+
+			System.out.println("Entre com o valor da posição: " +i);
+			vA[i] = scan.nextInt();
+
+			vB[i] = vA[i] * 2; 
+
+		}
+		
+		System.out.print("Vetor A = ");
+		for(int i = 0; i < vA.length; i++ ) {
+			System.out.print(vA[i]+ " ");
+		}
+		System.out.println();
+
+		System.out.print("Vetor B = ");
+		for(int i = 0; i < vB.length; i++ ) {
+			System.out.print(vB[i]+ " ");
+		}
+		System.out.println();
+	}
+
+}
