5d8968a8510f38810688f7f36ea8ae6f0ab87a2b
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-11-01 13:52:08 -0300

    Arrays - Exerc 07

---
+++ curso-java-basico/src/com/poli/cursojava/aula19/labs/Exerc07.java
@@ -0,0 +1,52 @@
+package com.poli.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exerc07 {
+
+	public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+		
+		int [] vA = new int [3];
+		int [] vB = new int [vA.length];
+		int [] vC = new int [vA.length];
+		
+		
+		for(int i = 0; i < vA.length; i++) {
+			System.out.println("Entre com o valor do Vetor A posição: " +i);
+			vA[i] = scan.nextInt();
+		}
+		
+		for(int i = 0; i < vB.length; i++) {
+			System.out.println("Entre com o valor do Vetor B posição: " +i);
+			vB[i] = scan.nextInt();
+		}
+		
+		for(int i = 0; i < vC.length; i++) {
+			vC[i] = vA[i] - vB[i];
+			System.out.println(vC[i] + " ");
+		}
+		
+		System.out.println();
+		
+		System.out.print("Vetor A = ");
+		for(int i = 0; i < vA.length; i++) {
+			System.out.print(vA[i] + " ");	
+		}
+		System.out.println();
+		
+		System.out.print("Vetor B = ");
+		for(int i = 0; i < vA.length; i++) {
+			System.out.print(vB[i] + " ");	
+		}
+		System.out.println();
+		
+		System.out.print("Vetor C = ");
+		for(int i = 0; i < vC.length; i++) {
+			System.out.print(vC[i] + " ");	
+		}
+		System.out.println();
+
+	}
+
+}
