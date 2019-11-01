1ee298e5fe5920e1bac098eb6b36115694d603b1
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-11-01 14:02:06 -0300

    Arrays - Exerc 09

---
+++ curso-java-basico/src/com/poli/cursojava/aula19/labs/Exerc09.java
@@ -0,0 +1,56 @@
+package com.poli.cursojava.aula19.labs;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exerc09 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int [] vA = new int [3];
+		int [] vB = new int [vA.length];
+		double [] vC = new double [vA.length];
+
+
+		for(int i = 0; i < vA.length; i++) {
+			System.out.println("Entre com o valor do Vetor A posição: " +i);
+			vA[i] = scan.nextInt();
+		}
+
+		for(int i = 0; i < vB.length; i++) {
+			System.out.println("Entre com o valor do Vetor B posição: " +i);
+			vB[i] = scan.nextInt();
+		}
+
+		DecimalFormat df = new DecimalFormat("###,###.##");
+		
+		for(int i = 0; i < vC.length; i++) {
+			vC[i] = vA[i] / vB[i];
+			System.out.println(vC[i] + " ");
+		}
+
+		System.out.println();
+
+		System.out.print("Vetor A = ");
+		for(int i = 0; i < vA.length; i++) {
+			System.out.print(vA[i] + " ");	
+		}
+		System.out.println();
+
+		System.out.print("Vetor B = ");
+		for(int i = 0; i < vA.length; i++) {
+			System.out.print(vB[i] + " ");	
+		}
+		System.out.println();
+
+		System.out.print("Vetor C = ");
+		for(int i = 0; i < vC.length; i++) {
+			System.out.print(vC[i] + " ");	
+		}
+		
+		System.out.println();
+
+	}
+
+}
