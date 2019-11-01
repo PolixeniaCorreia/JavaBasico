b526567ed9f077cf1ebd7443165007884c56ff55
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-11-01 14:06:47 -0300

    Arrays - Exerc 10

--- curso-java-basico/src/com/poli/cursojava/aula19/labs/Exerc09.java
+++ curso-java-basico/src/com/poli/cursojava/aula19/labs/Exerc09.java
@@ -12,7 +12,6 @@ public class Exerc09 {
 		int [] vB = new int [vA.length];
 		double [] vC = new double [vA.length];
 
-
 		for(int i = 0; i < vA.length; i++) {
 			System.out.println("Entre com o valor do Vetor A posição: " +i);
 			vA[i] = scan.nextInt();
@@ -36,6 +35,7 @@ public class Exerc09 {
 		for(int i = 0; i < vA.length; i++) {
 			System.out.print(vA[i] + " ");	
 		}
+		
 		System.out.println();
 
 		System.out.print("Vetor B = ");
