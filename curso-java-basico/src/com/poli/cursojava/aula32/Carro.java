1863203c69180b173141c8283562f912d9e8bb75
Polixenia Correia <polixeniacorreia@gmail.com>
2019-11-21 10:03:07 -0300

    Encapsulamento: métodos getters e setters

---
+++ curso-java-basico/src/com/poli/cursojava/aula32/Carro.java
@@ -0,0 +1,43 @@
+package com.poli.cursojava.aula32;
+
+public class Carro {
+	
+	private String marca;
+	private String modelo;
+	private int numPassageiros;
+	private double capComb;
+	private double consumoCombustivel;
+	
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getNumPassageiros() {
+		return numPassageiros;
+	}
+	public void setNumPassageiros(int numPassageiros) {
+		this.numPassageiros = numPassageiros;
+	}
+	public double getCapComb() {
+		return capComb;
+	}
+	public void setCapComb(double capComb) {
+		this.capComb = capComb;
+	}
+	public double getConsumoCombustivel() {
+		return consumoCombustivel;
+	}
+	public void setConsumoCombustivel(double consumoCombustivel) {
+		this.consumoCombustivel = consumoCombustivel;
+	}
+
+}
