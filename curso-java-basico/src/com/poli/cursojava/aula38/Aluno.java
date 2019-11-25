7782e1d76e028347521c85d252e46651cf807ef0
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-11-25 13:41:42 -0300

    Herança: palavra chave super

---
+++ curso-java-basico/src/com/poli/cursojava/aula38/Aluno.java
@@ -0,0 +1,42 @@
+package com.poli.cursojava.aula38;
+
+public class Aluno extends Pessoa{
+	
+	private String curso;
+	private double[] notas;
+	
+	
+	public Aluno() {
+		super();
+	}
+	public Aluno(String nome, String endereco, String telefone, String cpf, String telefoneCelular, String curso) {
+		super(nome, endereco, telefone, cpf, telefoneCelular);
+		this.curso = curso;
+	}
+	public Aluno(String curso, double[] notas) {
+		super();
+		this.curso = curso;
+		this.notas = notas;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public double[] getNotas() {
+		return notas;
+	}
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+	
+	public double calcularMedia() {
+		return 0;
+	}
+	
+	public boolean verificarAprovado() {
+		return true;
+	}
+	
+}
