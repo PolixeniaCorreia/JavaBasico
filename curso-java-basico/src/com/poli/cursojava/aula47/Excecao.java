96d709162a1b0ccaa6a9ad9ae4dca249c7a59da9
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-11-28 13:07:32 -0300

    Exceptions: try, catch

---
+++ curso-java-basico/src/com/poli/cursojava/aula47/Excecao.java
@@ -0,0 +1,37 @@
+package com.poli.cursojava.aula47;
+
+public class Excecao {
+	
+	public static void main (String[] args) {
+		
+		try {
+			int[] vetor = new int[4];
+			
+			System.out.println("Antes da exception.");
+			
+			vetor[4] = 1;
+			
+			
+			
+			
+		} catch (ArrayIndexOutOfBoundsException e) {
+			
+			System.out.println("Exceção ao acessar um índice do vetor que não existe.");	
+		}
+		
+		System.out.println("Esse texto será impresso.");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
